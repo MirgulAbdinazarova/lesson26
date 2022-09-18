2f36e2074e62137c7404ba1efd2ed9a0f10da226
User <mirgulabdinazarova@gmail.com>
2022-09-18 19:13:43 +0600

    commmit.home worke26

---
+++ src/Box.java
@@ -0,0 +1,12 @@
+public class Box<T> {
+
+public static <T> T getBox(T box){
+
+    return box;
+}
+
+    }
+
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
 
-        method(new  String[]{"Argen","hjhjj","jhdfgj","uytyt"});
-        method(new Integer[]{1,5,6,6,4,8,9,2,1});
+
+        System.out.println(Box.getBox("Box"));
     }
 
-    public  static <T>void method(T []arr) {
-        System.out.println(arr[0]+" ;"+arr[arr.length-1]);
-        for (T element:arr) {
-            System.out.println(element);
-        }
-    }
+
 }
